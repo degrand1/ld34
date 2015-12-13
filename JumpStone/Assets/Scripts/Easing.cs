9474dbfd74d9e6a4dcda21a67aa5224fdced14f1
David Yu <lewen.yu@gmail.com>
2015-12-12 18:02:14 -0800

    scripted movement

---
+++ JumpStone/Assets/Scripts/Easing.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Easing {
+	public class Quadratic {
+		public static float Out( float t ) {
+			return -t * ( t - 2 );
+		}
+
+		public static float In( float t ) {
+			return t * t;
+		}
+
+		public static float InOut( float t ) {
+			if ( t < 0.5 )
+				return 2 * t * t;
+			else
+				return -0.5f * ( ( 2*t-1 ) * ( 2*( t-1 )-2 ) - 1 );
+		}
+	}
+
+	public class Sine {
+		public static float Out( float t ) {
+			return Mathf.Sin (t * (Mathf.PI / 2));
+		}
+
+		public static float In( float t ) {
+			return 1 - Mathf.Sin ( t * (Mathf.PI / 2 ));
+		}
+
+		public static float InOut( float t ) {
+			if (t < 0.5)
+				return 0.5f * (Mathf.Sin (Mathf.PI * t));
+			else
+				return -0.5f * (Mathf.Cos (Mathf.PI * (2 * t - 1) / 2) - 2);
+		}
+	}
+}
--- JumpStone/Assets/Scripts/ScriptedMovement.cs
+++ JumpStone/Assets/Scripts/ScriptedMovement.cs
@@ -2,24 +2,85 @@ using UnityEngine;
 using System.Collections;
 
 public class ScriptedMovement : MonoBehaviour {
-	public enum Easing {
+	public enum EaseType {
+		Linear,
 		QuadInOut,
 		SineInOut,
 	};
 
-	public GameObject start;
-	public GameObject end;
+	public enum ScriptedMovementState {
+		P1_TO_P2,
+		PAUSE_P2,
+		P2_TO_P1,
+		PAUSE_P1,
+	};
+
+	public GameObject p1;
+	public GameObject p2;
 	public float duration = 1f;
 	public float pause = 0.5f;
-	public Easing easing = Easing.QuadInOut;
+	public EaseType easing = EaseType.QuadInOut;
+	public ScriptedMovementState state = ScriptedMovementState.PAUSE_P1;
+
+	private float acc = 0f;
 
 	void Start () {
-		if ( start == null || end == null ) {
-			Debug.Log( transform.gameObject.name + " scripted movement disabled! Start or End is null!" );
+		if ( p1 == null || p2 == null ) {
+			Debug.Log( transform.gameObject.name + " scripted movement disabled! P1 or P2 is null!" );
 			enabled = false;
+		} else {
+			transform.position = p1.transform.position;
 		}
 	}
 
 	void Update () {
+		acc += Time.deltaTime;
+
+		switch ( state ) {
+			case ScriptedMovementState.PAUSE_P1:
+				if ( acc > pause ) {
+					acc = 0f;
+					state = ScriptedMovementState.P1_TO_P2;
+				}
+				break;
+			case ScriptedMovementState.PAUSE_P2:
+				if ( acc > pause ) {
+					acc = 0f;
+					state = ScriptedMovementState.P2_TO_P1;
+				}
+				break;
+			case ScriptedMovementState.P1_TO_P2:
+				if ( acc < duration ) {
+					float t = ease( acc / duration );
+					transform.position = Vector3.Lerp( p1.transform.position, p2.transform.position, t );
+				} else {
+					acc = 0f;
+					state = ScriptedMovementState.PAUSE_P2;
+				}
+				break;
+			case ScriptedMovementState.P2_TO_P1:
+				if ( acc < duration ) {
+					float t = ease( acc / duration );
+					transform.position = Vector3.Lerp( p2.transform.position, p1.transform.position, t );
+				} else {
+					acc = 0f;
+					state = ScriptedMovementState.PAUSE_P1;
+				}
+				break;
+		}
+	}
+
+	float ease( float t ) {
+		switch ( easing ) {
+			case EaseType.Linear:
+				return t;
+			case EaseType.QuadInOut:
+				return Easing.Quadratic.InOut( t );
+			case EaseType.SineInOut:
+				return Easing.Sine.InOut( t );
+			default:
+				Debug.Log( "warning: " + easing + " not implemented in switch statement!" );
+				return t;
+		}
 	}
 }
